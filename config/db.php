46308cf6b94f5609b1b9210bdb96b2495836ab33
Nikolay Kostyurin <jilizart@gmail.com>
2025-01-02 21:31:20 +0200

    med

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
 	'class' => 'yii\db\Connection',
 	'dsn' => 'mysql:host=localhost;dbname=w3gh',
 	'username' => 'root',
-	'password' => '4549',
+	'password' => '',
 	'charset' => 'utf8',
 ];
